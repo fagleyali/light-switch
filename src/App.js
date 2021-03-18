a38a2d9d57149a747b803689333d7249c72f3539
Fagley Ali Hossain <fagleyali@Fagleys-MBP.home>
2021-03-18 17:33:39 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+
 import './App.css';
 
 function App() {
+  const [on, setOn] = useState(false)
+
+  const handleClick = () => {
+    setOn(!on)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Light Switch</h1>
+     <div className='light-switch' style={{backgroundColor:'brown', padding:'50px', display:'flex'}}>
+          <div  style={{backgroundColor:on? 'white' : 'gray', width:'300px', height:'100px',border:'1px solid rgba(0,0,0,0.5)', color:'red', fontSize:'50px', fontWeight:800, padding:30}}>{on ? 'ON' : 'OFF'} </div>
+          {/* <div style={{backgroundColor:'transparent', width:'300px', height:'100px', cursor:'pointer', border:'1px solid rgba(0,0,0,0.5)'}}></div> */}
+          <button type='button' onClick={handleClick}>Switch</button>
+     </div>
     </div>
   );
 }
